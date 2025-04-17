6738d347967bb543233c61d9aa4ed0f989334c8d
rizz_29 <ar.raza5092@gmail.com>
2025-04-17 10:54:05 +0500

    Photo input added

--- web-nexus/src/pages/gallery/gallery.js
+++ web-nexus/src/pages/gallery/gallery.js
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import {Button, Box} from '@mui/material'
 const gallery = () => {
+  const handleChange = () => {
+    
+  }
   return (
     <div>
-      
+      <Box>
+        <input
+          accept="image/*"
+          capture="environment"
+          style={{ display: 'none' }}
+          id="camera-input"
+          multiple
+          type="file"
+          onChange={handleChange}
+        />
+        <label htmlFor="camera-input">
+          <Button variant="contained" component="span" startIcon={<PhotoCameraIcon />}>
+            Take Picture
+          </Button>
+        </label>
+      </Box>
     </div>
   );
 }
